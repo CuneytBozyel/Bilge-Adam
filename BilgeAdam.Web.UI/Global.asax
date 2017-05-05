81c6998efce2b9a2f56bcc0a6ec8c561b1b6b947
cuneyt.bozyel <cuneyt.bozyel@NB0119>
2017-05-05 16:39:42 +0300

    Katmanlı Yapı

---
+++ BilgeAdam.Web.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BilgeAdam.Web.UI.MvcApplication" Language="C#" %>
---
+++ BilgeAdam.Web.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BilgeAdam.Web.UI.Startup))]
+namespace BilgeAdam.Web.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
